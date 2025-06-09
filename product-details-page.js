6b30c5ec7d2667cbb5400127123fa075026c9cf7
Viki Chan <chansinman1226@gmail.com>
2025-06-09 16:10:33 +0800

    Delete product-details-page.js

--- product-details-page.js
+++
@@ -1,9 +0,0 @@
-// 加入購物車按鈕功能
-document.getElementById('addToCartBtn').addEventListener('click', function() {
-    const sizeSelected = document.querySelector('input[name="size"]:checked') !== null;
-    if (sizeSelected) {
-        alert('已加入購物車');
-    } else {
-        alert('請選擇尺碼');
-    }
-});
